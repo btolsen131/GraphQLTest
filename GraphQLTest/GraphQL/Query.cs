622e8a60e29b2fb7e85e6dafbedcada10a6c4440
Brian Olsen <btolsen131@gmail.com>
2024-02-19 20:48:58 -0500

    Add query for commands and git ignroe update

--- GraphQLTest/GraphQL/Query.cs
+++ GraphQLTest/GraphQL/Query.cs
@@ -8,5 +8,10 @@ namespace GraphQLTest.GraphQL{
         public IQueryable<Platform> GetPlatform([Service] AppDbContext context){
             return context.Platforms;
         }
+
+        [UseProjection]
+        public IQueryable<Command> GetCommands(AppDbContext context){
+            return context.Commands;
+        }
     }
 }
